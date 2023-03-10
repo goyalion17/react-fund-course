f87ac8c6633bf5e4e128345acc80e3a451d74362
Irina Saveleva <goyalion17@gmail.com>
2023-03-10 18:02:39 +0100

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
-import { ClassCounter } from './components/ClassCounter';
-import { Counter } from './components/Counter';
 import PostItem from './components/PostItem';
 import './styles/App.css';
 
 function App() {
-  const [value, setValue] = useState('Text in input')
+  const [posts, setPost] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'Javascript 2', body: 'Description'},
+    {id: 3, title: 'Javascript 3', body: 'Description'},
+  ])
   
- 
   return (
-    <div className="App">   
-      {/* <Counter />      
-      <ClassCounter />   */}   
-      <PostItem/>
-      <PostItem/>
-      <PostItem/>
-      <PostItem/>
-      <PostItem/>
+    <div className="App">
+      <h1 style={{textAlign: 'center'}}>List of posts</h1>
+      {posts.map(post =>
+        <div className="">
+          <PostItem post={post} key={post.id} />
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
-function PostItem() {
+const PostItem = (props: any) => {
   return (
-    <div className="App">   
       <div className="post">
         <div className="post__content">
-          <strong>1. Javascript</strong>
-          <div>Javascript is programming language</div>
+          <strong>{props.post.id}. {props.post.title}</strong>
+          <div>{props.post.body}</div>
         </div>
         <div className="post__btn">
           <button>Delete</button>
         </div>
       </div> 
-    </div>
   );
 }
 
